b0b608e9cd103567347c466918696e0fe39bc7c1
阿洧 <a65162@gmail.com>
2019-02-27 15:35:37 +0800

    add sitemap

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
-    mode: 'spa',
+    mode: "spa",
 
     /*
      ** Headers of the page
@@ -9,58 +9,80 @@ module.exports = {
     head: {
         title: "basic_nuxt_project",
         meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
-            { hid: 'description', name: 'description', content:''}
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1, user-scalable=no"
+            },
+            { hid: "description", name: "description", content: "" }
         ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
     },
 
     /*
      ** Customize the progress-bar color
      */
-    loading: { color: '#008AC6' },
+    loading: { color: "#008AC6" },
 
     /*
      ** Global CSS
      */
     css: [
-        'element-ui/lib/theme-chalk/index.css',
-        'css-reset-and-normalize/css/reset-and-normalize.css'
+        "element-ui/lib/theme-chalk/index.css",
+        "css-reset-and-normalize/css/reset-and-normalize.css"
     ],
 
     /*
      ** Plugins to load before mounting the App
      */
     plugins: [
-        '@/plugins/element-ui',
-        '@/plugins/global-components',
-        '@/plugins/global-mixins',
-        '@/plugins/fontawesome',
+        "@/plugins/element-ui",
+        "@/plugins/global-components",
+        "@/plugins/global-mixins",
+        "@/plugins/fontawesome"
     ],
 
     /*
      ** Nuxt.js modules
      */
     modules: [
-        '@nuxtjs/axios',
-        'nuxt-sass-resources-loader'
+        "@nuxtjs/axios",
+        "nuxt-sass-resources-loader",
+        // [
+        //     "nuxt-i18n",
+        //     {
+        //         locales: [
+        //             {
+        //                 code: "en",
+        //                 iso: "en-US"
+        //             },
+        //             {
+        //                 code: "zh-TW",
+        //                 iso: "zh-TW"
+        //             }
+        //         ],
+        //         defaultLocale: "zh-TW",
+        //         vueI18nLoader: true,
+        //         // baseUrl: "http://www.fucosolution.com.tw"
+        //     }
+        // ],
+        "@nuxtjs/sitemap",
     ],
-    sassResources: [
-        '@/assets/scss/style.scss'
-    ],
-    axios: {
-
+    sassResources: ["@/assets/scss/style.scss"],
+    axios: {},
+    sitemap: {
+        path: "/sitemap.xml",
+        // hostname: "http://www.fucosolution.com.tw",
+        cacheTime: 1000 * 60 * 15,
+        gzip: true,
+        generate: false // Enable me when using nuxt generate
     },
     router: {
-        extendRoutes (routes, resolve) {
+        extendRoutes(routes, resolve) {
             // routes.forEach(route => {
             //     switch (route.name) {
             //         case 'index':
             //             break;
-                
             //         default:
             //             break;
             //     }
@@ -71,7 +93,7 @@ module.exports = {
      ** Build configuration
      */
     build: {
-        vendor: ['jquery'],
+        vendor: ["jquery"],
         plugins: [
             new webpack.ProvidePlugin({
                 $: "jquery"
@@ -88,8 +110,8 @@ module.exports = {
                     test: /\.(js|vue)$/,
                     loader: "eslint-loader",
                     exclude: /(node_modules)/
-                })
+                });
             }
         }
     }
-}
+};
